4f9573d4765d8f72ae5ced809ab9c3f06ca8910d
siddhikhapare <siddhikhapare66@bvcoenm.edu.in>
2022-01-11 15:49:03 +0530

    send a message using twilio

---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTHTOKEN;
+const toNumber = process.env.TWILIO_TO_NUMBER;
+const fromNumber = process.env.TWILIO_FROM_NUMBER;
+
+const client = require('twilio')(accountSid, authToken);
+client.messages
+      .create({body: 'Hi there', 
+      from: fromNumber, 
+      to: toNumber
+    })
+      .then(message => console.log(message.sid))
+      .catch((err) => console.log(err))
+
+
+
+
